fd538050b9853014860c63eacb44e91894c510a7
RavengerINC <jamesmdalziel@gmail.com>
2026-01-30 14:04:39 -0400

    Added reel to TV page

--- src/Components/Tv.js
+++ src/Components/Tv.js
@@ -4,6 +4,18 @@ import Section from './Section/Section';
 function Tv() {
     return (
         <div className="Tv">
+            <div className="Tv-reel">
+                <iframe 
+                    width="100%" 
+                    height="500" 
+                    src="https://www.youtube.com/embed/8KT9egsN1yo?si=eQF4GEigb8zGSpk8" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>                    
+                </iframe>
+            </div>
             <Section
                 searchText="tv-animation"
             />
